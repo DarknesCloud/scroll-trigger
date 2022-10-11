d60e2d0d6cfc42e8cfd570b295d4d0048def55aa
Saul Leiva <aldairleiva42@gmail.com>
2022-10-11 15:21:33 -0600

    styling v2.

--- src/App.tsx
+++ src/App.tsx
@@ -5,86 +5,80 @@ function App() {
   const size = useWindowsSize();
 
   return (
-    <div className="App">
+    <div>
       <div className="container">
         <div className="intro">
           <div className="nav">
-            <div className="nav__logo">
-              <ul className="navigation">
-                <li className="navigation__item">
-                  <a href="" className="navigation__link">
-                    Instagram
-                  </a>
-                </li>
-                <li className="navigation__item">
-                  <a href="" className="navigation__link">
-                    Facecbook
-                  </a>
-                </li>
-                <li className="navigation__item">
-                  <a href="" className="navigation__link">
-                    Twitter
-                  </a>
-                </li>
-                <li className="navigation__item">
-                  <a href="" className="navigation__link">
-                    Pinterest
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <div className="nav--logo">m.</div>
+            <ul className="navigation">
+              <li className="navigation__item">
+                <a href="" className="navigation__link">
+                  Instagram
+                </a>
+              </li>
+              <li className="navigation__item">
+                <a href="" className="navigation__link">
+                  Twitter
+                </a>
+              </li>
+              <li className="navigation__item">
+                <a href="" className="navigation__link">
+                  Youtube
+                </a>
+              </li>
+              <li className="navigation__item">
+                <a href="" className="navigation__link">
+                  Pinterest
+                </a>
+              </li>
+            </ul>
           </div>
           <div className="intro-center">
             <span>Clothing</span>
             <figure>
-              <img src="https://picsum.photos/200/300" alt="" />
+              <img src="https://picsum.photos/200/300" />
             </figure>
             <span className="top">Imported</span>
             <div className="text">
-              in Accra <span>0'03'423'2</span>
+              in Accra <span>0'03'439'02</span>
             </div>
           </div>
 
           <div className="intro-bottom">
-            <div className="intro-bottom-list">Volume 02</div>
+            <div className="intro-bottom-list">Vomule 02</div>
             <div className="intro-bottom-list">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut est
-              eveniet officia eos aspernatur dolor beatae atque, quisquam
-              consequatur tenetur soluta ea pariatur fugit asperiores a
-              explicabo cum suscipit voluptatem.
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
             </div>
             <div className="intro-bottom-list">By Lorem Ipsum</div>
           </div>
         </div>
-
         <div className="section section--one">
-          <figure>
-            <img src="https://picsum.photos/200/300" />
+          <figure className="img-bg">
+            <img src="https://images.pexels.com/photos/1525041/pexels-photo-1525041.jpeg" />
           </figure>
           <div className="wrapper-img">
             <figure className="img-reveal">
-              <img src="" />
+              {/* <img src="https://picsum.photos/200/300" /> */}
             </figure>
           </div>
         </div>
-
-        <div className="section section--two">
+        <div className="section section--tow">
           <h1>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab iusto
-            nihil quidem aperiam enim. Sed itaque quibusdam debitis! Eligendi
-            delectus itaque ab similique ducimus dolorem. Quibusdam animi itaque
-            dolore quis.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut numquam
+            eum accusantium? Dolor officiis beatae dolorem expedita eligendi
+            quidem commodi fugit impedit accusamus consectetur, quos
+            perspiciatis aut, nemo officia asperiores?
           </h1>
           <figure>
             <img src="https://picsum.photos/200/300" />
           </figure>
         </div>
-        <div className="section section-third">
+        <div className="section section--third">
           <h1>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            omnis officia earum aperiam quibusdam doloribus doloremque nisi
-            nihil dolores velit, expedita, reprehenderit atque quia, sint
-            recusandae quam eos nobis eius.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+            iste quisquam. Perferendis assumenda aliquam ratione incidunt
+            tempora eaque obcaecati ducimus illo nostrum reiciendis quas, fuga
+            iure saepe. Repellat, aperiam laudantium?
           </h1>
         </div>
       </div>
